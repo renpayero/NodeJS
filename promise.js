d30e84155eece62293a6a9ac6dcb2ae84467c5cc
Renzo Payero <renzopayero@hotmail.com>
2023-09-24 23:02:47 -0300

    curso terminado

--- promise.js
+++ promise.js
@@ -1,25 +1,40 @@
-const {readFile} = require('fs')
+const {readFile} = require('fs/promises')
 
 
+// let getText = function(pathFile){
+//     return new Promise(function(resolve, reject){
+//         readFile('./data/first.txt', 'utf-8', function(error,data){
+//             if (error){
+//                 reject(error)
+//             }
+//             resolve(data)
+//         })
+//     })
+// }
 
-let getText = function(pathFile){
-    return new Promise(function(resolve, reject){
-        readFile('./data/first.txt', 'utf-8', function(error,data){
-            if (error){
-                reject(error)
-            }
-            resolve(data)
-        })
-    })
-}
-
-let resultado = function(result){
-    console.log(result)
-}
-let errorF = function(){
-    console.log(error)
-}
+// let resultado = function(result){
+//     console.log(result)
+// }
+// let errorF = function(){
+//     console.log(error)
+// }
 // getText('./data/first.txt').then(resultado()).catch(errorF())
 
-getText('./data/first.txt').then((result)=>{console.log(result)}).catch((error)=>{console.log(error)})
+// getText('./data/first.txt').then((result)=>{console.log(result)}).catch((error)=>{console.log(error)})
 
+async function read(){
+    try{
+        const result = await readFile('./data/first.txt', 'utf-8')
+        console.log(result)
+        const restult2 = await readFile('./data/second.txt', 'utf-8')
+        console.log(restult2)
+        const result3 = await readFile('./data/newfile.txt', 'utf-8')
+        console.log(result3)
+        const result4 = await readFile('./data/third.txt', 'utf-8')
+        console.log(result4)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+read()
